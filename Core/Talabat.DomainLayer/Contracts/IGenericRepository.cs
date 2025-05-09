9fbd141644a263f6ad071ca0f312ebeb27173a35
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-09 20:18:44 +0300

    Generi Repository

---
+++ Core/Talabat.DomainLayer/Contracts/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Tar;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.DomainLayer.Models;
+
+namespace Talabat.DomainLayer.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        // GetAll
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        // GetById
+        Task<TEntity?> GetByIdAsync(TKey id);
+        // Add
+        Task AddAsync(TEntity entity);
+        //Update 
+        void Update(TEntity entity);
+        // Remove 
+        void Remove(TEntity entity);
+    }
+}
---
+++ Infrastructure/Talabat.Persistence/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.DomainLayer.Contracts;
+using Talabat.DomainLayer.Models;
+using Talabat.Persistence.Data.DbContexts;
+
+namespace Talabat.Persistence.Data.Repositories
+{
+    public class GenericRepository<TEnitiy, TKey>(StoreDbContext _dbContext) : IGenericRepository<TEnitiy, TKey> where TEnitiy : BaseEntity<TKey>
+    {
+        public async Task AddAsync(TEnitiy entity)
+        {
+            await _dbContext.Set<TEnitiy>().AddAsync(entity);
+        }
+
+
+        public async Task<IEnumerable<TEnitiy>> GetAllAsync()
+        {
+            return await _dbContext.Set<TEnitiy>().ToListAsync();
+        }
+
+        public async Task<TEnitiy?> GetByIdAsync(TKey id)
+        {
+            return await _dbContext.Set<TEnitiy>().FindAsync(id);
+        }
+
+        public void Remove(TEnitiy entity)
+        {
+            _dbContext.Set<TEnitiy>().Remove(entity);
+        }
+
+        public void Update(TEnitiy entity)
+        {
+            _dbContext.Set<TEnitiy>().Update(entity);
+        }
+    }
+}
