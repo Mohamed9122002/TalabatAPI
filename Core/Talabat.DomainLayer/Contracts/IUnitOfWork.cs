2982b8d9992b3a1b8283799019eed877a661eeee
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-09 20:53:56 +0300

    UnitOfWork

---
+++ Core/Talabat.DomainLayer/Contracts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.DomainLayer.Models;
+
+namespace Talabat.DomainLayer.Contracts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, TKey> GenericRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>;
+        Task<int> SaveChangeAsync(); 
+
+    }
+}
