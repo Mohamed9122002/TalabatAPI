82d6b9f23d533b2bccb9927769da6d2150a494d5
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-23 00:35:17 +0300

    AuthenticationController

---
+++ Infrastructure/Talabat.Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.ServiceAbstraction;
+using Talabat.Shared.IdentityDTO;
+
+namespace Talabat.Presentation.Controllers
+{
+   public class AuthenticationController(IServiceManager serviceManager) :BaseApiController
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> LoginAsync(LoginDto loginDto)
+        {
+            return Ok(await serviceManager.AuthenticationService.LoginAsync(loginDto));
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> RegisterAsync(RegisterDto registerDto)
+        {
+            return Ok(await serviceManager.AuthenticationService.RegisterAsync(registerDto));
+        }
+    }
+}
