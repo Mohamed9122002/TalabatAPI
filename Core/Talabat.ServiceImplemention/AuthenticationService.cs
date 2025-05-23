3092b575d1af531e02d5131d447c9757f771e9ac
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-23 12:45:03 +0300

    Create Token

--- Core/Talabat.ServiceImplemention/AuthenticationService.cs
+++ Core/Talabat.ServiceImplemention/AuthenticationService.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Talabat.DomainLayer.Contracts.IdentityModule;
@@ -11,8 +15,9 @@ using Talabat.Shared.IdentityDTO;
 
 namespace Talabat.ServiceImplemention
 {
-    public class AuthenticationService(UserManager<ApplicationUser> _userManager) : IAuthenticationService
+    public class AuthenticationService(UserManager<ApplicationUser> _userManager, IConfiguration _configuration) : IAuthenticationService
     {
+
         public async Task<UserDto> LoginAsync(LoginDto loginDto)
         {
             // check if the user is exist or not 
@@ -25,7 +30,7 @@ namespace Talabat.ServiceImplemention
                 {
                     DispalyName = User.DisplayName,
                     Email = User.Email,
-                    Token = string.Empty // TODO: Generate Token
+                    Token = await CreateTokenAsync(User)
                 };
             }
             else
@@ -33,6 +38,8 @@ namespace Talabat.ServiceImplemention
                 throw new UnauthorizedException();
             }
         }
+
+
 
         public async Task<UserDto> RegisterAsync(RegisterDto registerDto)
         {
@@ -49,13 +56,44 @@ namespace Talabat.ServiceImplemention
             // Return UserDto 
             if (Result.Succeeded)
             {
-                return new UserDto { DispalyName = User.DisplayName, Email = User.Email, Token = string.Empty }; // TODO: Generate Token
+                return new UserDto { DispalyName = User.DisplayName, Email = User.Email, Token = await CreateTokenAsync(User) };
             }
             else
             {
                 var Errors = Result.Errors.Select(x => x.Description).ToList();
                 throw new BadRequestException(Errors);
             }
+        }
+        private async Task<string> CreateTokenAsync(ApplicationUser user)
+        {
+            // Payload
+            var Claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email,user.Email!),
+                new Claim(ClaimTypes.Name,user.UserName!),
+                new Claim(ClaimTypes.NameIdentifier,user.Id),
+                //new Claim(ClaimTypes.Role,user.Role),
+
+            };
+            var Roles = await _userManager.GetRolesAsync(user);
+            foreach (var Role in Roles)
+            {
+                Claims.Add(new Claim(ClaimTypes.Role, Role));
+            }
+            // Key 
+            var SecretKey = _configuration.GetSection("JWTOptions")["SecretKey"];
+            var Key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            // Credentials 
+            var Credentials = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+            // Create Token 
+            var Token = new JwtSecurityToken(
+                issuer: _configuration["JWTOptions:Issuer"],
+                audience: _configuration["JWTOptions:Audience"],
+                claims: Claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: Credentials);
+            // Return Token
+            return new JwtSecurityTokenHandler().WriteToken(Token);
         }
     }
 }
--- Core/Talabat.ServiceImplemention/ServiceManager.cs
+++ Core/Talabat.ServiceImplemention/ServiceManager.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,11 @@ using Talabat.ServiceAbstraction;
 
 namespace Talabat.ServiceImplemention
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository, UserManager<ApplicationUser> userManager) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository, UserManager<ApplicationUser> userManager ,IConfiguration configuration) : IServiceManager
     {
         private readonly Lazy<IProductService> _LazyProductService = new Lazy<IProductService>(() => new ProductService(_unitOfWork, _mapper));
         private readonly Lazy<IBasketService> _LazyBasketService = new Lazy<IBasketService>(() => new BasketService(_basketRepository, _mapper));
-        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager));
+        private readonly Lazy<IAuthenticationService> _LazyAuthenticationService = new Lazy<IAuthenticationService>(() => new AuthenticationService(userManager ,configuration));
 
         public IProductService ProductService => _LazyProductService.Value;
 
