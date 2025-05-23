ca1e7cb4745fe01a7c427796bba3d959ddb4274b
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-05-23 20:57:35 +0300

    OrderModuleEntities

---
+++ Core/Talabat.DomainLayer/Models/OrderModule/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.DomainLayer.Models;
+
+namespace Talabat.DomainLayer.Models.OrderModule
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public OrderAddress Address { get; set; } = default!;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+        // Fk 
+        public int DeliveryMethodId { get; set; }
+        public OrderStatus OrderStatus { get; set; } = default!;
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        public decimal GetTotal()
+        {
+            return SubTotal + DeliveryMethod.Price;
+        }
+    }
+}
